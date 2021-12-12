0f8e1f23820a5bc55a3a25e70b5064870802bb00
VIPUL KUMAR <54803468+vipulkumar5@users.noreply.github.com>
2021-12-13 00:07:55 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[]args){
+        int x=12;
+        int y=8;
+        int z=x+y;
+        int a=x-y;
+        float d=x*y;
+        double f = x%y;
+        System.out.println("add two number = "+(x+y));
+        System.out.println("add two number ="+z);
+        System.out.println("subtract two number =" +a);
+        System.out.println("multiply two number=" +d);
+        System.out.println("per two number=" +f);
+    }
+}
